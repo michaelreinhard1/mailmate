ad5c2d2ed0dd1189ef85955a5e4256f0f62a3072
Michael Reinhard <michael.reinhard.91@gmail.com>
2023-04-06 15:01:26 +0200

    Update

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -156,7 +156,7 @@ const emailRoutes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes: [...Authroutes, ...emailRoutes],
   linkActiveClass: "active-link",
 });
